1e5d72e92566ad20e04077f88bbf1cdc2dcfc783
DalgatAhi <156659789+DalgatAhi@users.noreply.github.com>
2024-10-31 21:09:25 +0300

    init

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import { Accordeon } from "../../components/accordeon/Accordeon"
+import { Banner } from "../../components/banner/Banner"
+import { PauseOnHover } from "../../components/slider/Slider"
+
+export function HomePage() {
+    return (
+        <section>
+            <Banner />
+            <div className="slider container">
+                <h2>Актуальные товары</h2>
+                <PauseOnHover />
+            </div>
+            <Accordeon />
+
+        </section>
+    )
+}
